6aa2477c2fef79f221af0d717d973579e5e3cd71
Je0ngye0n <wjddus9327@naver.com>
2024-06-30 02:09:54 +0900

    add : 시계 움직임 세팅
    시계 오브젝트 및 움직임 스크립트 초기 버전 세팅

---
+++ Assets/KJY/Script/MoveClock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClock : MonoBehaviour
+{
+    public Transform objHour;
+    public Transform objMinute;
+    public Transform objSecond;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
